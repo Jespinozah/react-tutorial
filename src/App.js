23ff25db26b9e08d277bd12e4b13abf4fed02af4
Brayan Espinoza <bjespinozah@gmail.com>
2018-03-11 11:12:31 -0500

    Clena files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
-import Button from './components/Button';
-import Result from './components/Result';
 import './App.css';
 
 class App extends Component {
-  state = {counter: 0};
 
-  incrementCounter = (incrementValue)=>{
-    this.setState((prevState)=> ({
-            counter: this.state.counter +incrementValue
-      }));
-  }
+  
   render() {
     return (
       <div className="App">
-        <Button incrementValue ={1} onClickFunction ={this.incrementCounter}/>
-        <Button incrementValue ={2} onClickFunction ={this.incrementCounter}/>
-        <Button incrementValue ={3} onClickFunction ={this.incrementCounter}/>
-        <Button incrementValue ={4} onClickFunction ={this.incrementCounter}/>
-        <Result counterPass = {this.state.counter}/>
+        <h1>GITHUB CARD</h1>
       </div>
     );
   }
