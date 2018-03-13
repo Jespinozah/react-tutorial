1fc2f6bd8a364d9c5d16a97c7851f9e324278db1
Brayan Espinoza <bjespinozah@gmail.com>
2018-03-12 22:26:30 -0500

    Clean files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React, { Component } from 'react';
-import CardList from './components/CardList';
-import Form from './components/Form';
+
 import './App.css';
 
 class App extends Component {
 
-  state = {
-    cards : [
-      
-     ]
-  };
-  addNewCard = (cardInfo)=>{
-    this.setState(prevState =>({
-        cards: prevState.cards.concat(cardInfo)
-      }));
-  };
+
 
   render() {
     return (
       <div className="App">
-        <h1>GITHUB CARD</h1>
-        <Form onSubmit={this.addNewCard}/>
-        <CardList cards={this.state.cards}/>
+          hola
       </div>
     );
   }
